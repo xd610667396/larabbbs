28f62b6ee7b87d0bb6142e6db8549609adf3dc1e
vagrant <vagrant@homestead>
2019-08-06 08:09:08 +0000

    Category topic list

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,7 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 //只能 show  update  edit这几种方式请求
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
-
 Route::get('/users/{user}', 'UsersController@show')->name('users.show');
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
